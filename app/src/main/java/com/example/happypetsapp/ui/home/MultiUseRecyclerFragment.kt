abc96f5376270e13a06be08662ae5c54e2906d40
Arthurs101 <84546401+Arthurs101@users.noreply.github.com>
2022-10-14 07:42:12 -0600

    Added some Layouts
    Algunos Layouts creados para la app

--- app/src/main/java/com/example/happypetsapp/ui/home/MultiUseRecyclerFragment.kt
+++ app/src/main/java/com/example/happypetsapp/ui/home/MultiUseRecyclerFragment.kt
@@ -1,17 +1,15 @@
 package com.example.happypetsapp.ui.home
-
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.happypetsapp.databinding.FragmentHomeBinding
+import com.example.happypetsapp.databinding.FragmentRecyclerMultiuseBinding
 
-class HomeFragment : Fragment() {
+class MultiUseRecyclerFragment : Fragment() {
 
-    private var _binding: FragmentHomeBinding? = null
+    private var _binding: FragmentRecyclerMultiuseBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -24,14 +22,8 @@ class HomeFragment : Fragment() {
     ): View {
         val homeViewModel =
             ViewModelProvider(this).get(HomeViewModel::class.java)
-
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        _binding = FragmentRecyclerMultiuseBinding.inflate(inflater, container, false)
         val root: View = binding.root
-
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         return root
     }
 
